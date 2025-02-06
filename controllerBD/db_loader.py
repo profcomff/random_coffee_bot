4fa36052c959e2072dc54666cfec5ff71f0ae94a
Stanislav Roslavtsev <roslavtcev.sv22@physics.msu.ru>
2025-02-06 12:06:29 +0300

    with Session() as db_session my ass

--- controllerBD/db_loader.py
+++ controllerBD/db_loader.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from data.config import DB_DSN
 
 engine = create_engine(DB_DSN, pool_pre_ping=True, isolation_level='AUTOCOMMIT')
-db_session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
